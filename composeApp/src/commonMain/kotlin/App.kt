67a7c228ee997e9cbb9b8471236931b3e2d56943
Yasuyuki Sugitani <sugitani@bcc.cc>
2024-04-13 23:37:43 +0900

    first commit

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,15 +1,26 @@
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -18,20 +29,30 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
+    val focusManager = LocalFocusManager.current
     var text by remember { mutableStateOf("") }
+    var background by remember { mutableStateOf(Color.LightGray) }
 
-    MaterialTheme {
-        BasicTextField(
+    Box(
+        modifier =
+        Modifier
+            .imePadding()
+            .fillMaxWidth()
+            .height(100.dp)
+            .background(background)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = {
+                    background = Color.Red
+                    focusManager.clearFocus()
+                },
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        OutlinedTextField(
             value = text,
             onValueChange = { text = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.LightGray)
-                .padding(18.dp),
-            maxLines = Int.MAX_VALUE,
-            textStyle = TextStyle.Default.copy(
-                textAlign = TextAlign.Center,
-            ),
         )
     }
 }
